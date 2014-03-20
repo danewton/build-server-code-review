05c6b2334b40d1a0488da67d658826f8c770092a
Tim Vollmer <timjvollmer@gmail.com>
2014-03-20 14:02:39 -0500

    returned to working echo state.

--- bin/code-reviews.sh
+++ bin/code-reviews.sh
@@ -8,8 +8,9 @@ rtnfile=/tmp/rtn$$
 
 PROC_CNT="8"
 RPLCMENT='{}';
+SH_CMD='TMP_LOG=/tmp/log$$; '$SCRIPT_DIR'/script-runner.sh {} '$rtnfile' >> $TMP_LOG 2>&1; cat $TMP_LOG'
 
-find $REVIEW_HOME/rules.d/*.sh | xargs -P $PROC_CNT -n 1 -I "$RPLCMENT" $SCRIPT_DIR/script-runner.sh "$RPLCMENT" $rtnfile
+find $REVIEW_HOME/rules.d/*.sh | xargs -P $PROC_CNT -n 1 -I "$RPLCMENT" sh -c "$SH_CMD"
 
 if [ -f $rtnfile ]; then
   rtn=$(cat $rtnfile)
