8fe85b2a63753b037fba621b3f26896e0d943492
Tim Vollmer <timjvollmer@gmail.com>
2014-03-21 09:26:12 -0500

    added rule to help the scripts locate the proper version element.

---
+++ rules.d/pom-needs-name-attr-to-properly-identify-version.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+. $(dirname $0)/../bin/setenv.sh
+
+if [ "$IS_MVN" == "false" ]; then
+ exit 0
+fi
+
+if [ $(grep -c '<name>' pom.xml) -ne 0 ]; then
+  echo "please add a <name> element in your pom to help our scripts to properly identify the correct <version> element within your pom.  Preferably, it would be within 4 or 5 lines above your <version> element  (this is a crappy parser, I know)."
+  exit 1
+fi
+
+
