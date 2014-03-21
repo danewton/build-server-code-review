68099a3f519d380d17d9a3fcd78f68d4d51c3136
Tim Vollmer <timjvollmer@gmail.com>
2014-03-21 09:29:15 -0500

    patched.

--- rules.d/pom-needs-name-attr-to-properly-identify-version.sh
+++ rules.d/pom-needs-name-attr-to-properly-identify-version.sh
@@ -6,7 +6,7 @@ if [ "$IS_MVN" == "false" ]; then
  exit 0
 fi
 
-if [ $(grep -c '<name>' pom.xml) -ne 0 ]; then
+if [ $(grep -c '<name>' pom.xml) -eq 0 ]; then
   echo "please add a <name> element in your pom to help our scripts to properly identify the correct <version> element within your pom.  Preferably, it would be within 4 or 5 lines above your <version> element  (this is a crappy parser, I know)."
   exit 1
 fi
