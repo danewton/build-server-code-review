f0a935ee8559d16d036a22cbef25fe80752710bc
Tim Vollmer <timjvollmer@gmail.com>
2014-03-21 09:17:36 -0500

    bug fix for scm matching.

--- rules.d/mvn-should-have-matching-scm.sh
+++ rules.d/mvn-should-have-matching-scm.sh
@@ -13,7 +13,9 @@ fi;
 
 POM_SCM_URL=$(getPomSCM)
 SVN_SCM_URL=$(svn info | grep URL | cut -c 6-  | sed 's/\(.*\)\(tags\)\(.*\)/\1/g'  | sed 's/\(.*\)\(trunk\)\(.*\)/\1/g'  | sed 's/\(.*\)\(branches\)\(.*\)/\1/g')
-if [ "$SVN_SCM_URL" != "POM_SCM_URL" ]; then
+if [ "$SVN_SCM_URL" != "$POM_SCM_URL" ]; then
+ echo "svn : $SVN_SCM_URL"
+ echo "pom : $POM_SCM_URL"
  echo "The SCM/SVN url from the checkout does not match the SCM/SVN url that's in the POM.  Please add or correct it, so that the mvn release:release plan can properly tag the project."
  exit 1
 fi
