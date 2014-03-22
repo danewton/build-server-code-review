1c14a1ef570df343b7c4daf1a91f5bee5acf40c8
Tim Vollmer <timjvollmer@gmail.com>
2014-03-21 21:02:15 -0500

    updated error message.

--- rules.d/src-should-not-have-system-printlns.sh
+++ rules.d/src-should-not-have-system-printlns.sh
@@ -3,7 +3,7 @@
 . $(dirname $0)/../bin/setenv.sh
 
 if [ $(grep -R 'System.out.println' $SRCDIR | wc -l) -ne 0 ]; then
-  echo "please remove the System.out.println calls from the java source directory."
+  echo "Please remove the System.out.println calls from the java source directory, and use a Logger to write to the application logs instead."
   exit 1
 fi
 
