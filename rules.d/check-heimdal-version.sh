4fd3697825538257a04381b6065b03e131818535
Tim Vollmer <timjvollmer@gmail.com>
2014-03-21 21:09:16 -0500

    added exclusion for heimdall; don't need to check its heimdall version.

--- rules.d/check-heimdal-version.sh
+++ rules.d/check-heimdal-version.sh
@@ -6,6 +6,11 @@ if [ "$IS_MVN" == "false" ]; then
  exit 0
 fi
 
+ARTIFACT_ID=$(getArtifactId)
+if [ "$ARTIFACT_ID" == "heimdall" ]; then
+ exit 0
+fi
+
 minVersion "heimdal" "1.0.3"
 if [ $? -ne 0 ]; then
  exit 1
