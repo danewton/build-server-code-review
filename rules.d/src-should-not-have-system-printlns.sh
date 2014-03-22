c18fc72d51942579394024abccf96057e7fff134
Tim Vollmer <timjvollmer@gmail.com>
2014-03-21 20:52:23 -0500

    repaired check for System.out.println statements.

--- rules.d/src-should-not-have-system-printlns.sh
+++ rules.d/src-should-not-have-system-printlns.sh
@@ -2,8 +2,8 @@
 
 . $(dirname $0)/../bin/setenv.sh
 
-if [ $(grep -R 'System.println' $SRCDIR | wc -l) -ne 0 ]; then
-  echo "please remove the System.println calls from the java source directory."
+if [ $(grep -R 'System.out.println' $SRCDIR | wc -l) -ne 0 ]; then
+  echo "please remove the System.out.println calls from the java source directory."
   exit 1
 fi
 
