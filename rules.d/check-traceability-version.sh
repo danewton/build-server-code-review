4bba83f7c3a1a92893c071e4782510f22cdbec7c
Tim Vollmer <timjvollmer@gmail.com>
2014-03-26 14:46:09 -0500

    added rule for min traceability version.

---
+++ rules.d/check-traceability-version.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+. $(dirname $0)/../bin/setenv.sh
+
+if [ "$IS_MVN" == "false" ]; then
+ exit 0
+fi
+
+rtn=0
+
+minVersion "traceability" "1.0.7"
+if [ $? -ne 0 ]; then
+ rtn=1
+fi
+
+
+exit $rtn
+
