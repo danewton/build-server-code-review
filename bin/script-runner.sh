6d3783ec13a8e8d1d3f1ddef7f0e346bfba3f82b
Tim Vollmer <timjvollmer@gmail.com>
2014-03-31 10:45:31 -0500

    added trap to clean logs, refactored local variable name.

--- bin/script-runner.sh
+++ bin/script-runner.sh
@@ -8,29 +8,32 @@ REVIEW_HOME=$SCRIPT_DIR/../
 SCRIPT_OUT=/tmp/out$$;
 TMP_LOG=/tmp/log$$;
 
-if [ -f "$SCRIPT_OUT" ]; then
- rm "$SCRIPT_OUT";
-fi
+function cleanLogs(){
+ if [ -f "$SCRIPT_OUT" ]; then
+  rm "$SCRIPT_OUT";
+ fi
 
-if [ -f "$TMP_LOG" ]; then
- rm "$TMP_LOG";
-fi
+ if [ -f "$TMP_LOG" ]; then
+  rm "$TMP_LOG";
+ fi
+}
 
-HEAD_START="$(basename $1)"
+STATUS_LINE_START="$(basename $1)"
 
+trap "cleanLogs" EXIT
+
+cleanLogs; # start with a clean slate
 $1 $@ >> $SCRIPT_OUT 2>&1;
 if [ "$?" -ne 0 ]; then
- printf "%-75s%-15s\n" $HEAD_START "FAILED" >> $TMP_LOG
+ printf "%-75s%-15s\n" $STATUS_LINE_START "FAILED" >> $TMP_LOG
  echo "1" > $2;
 else
- printf "%-75s%-15s\n" $HEAD_START "PASS" >> $TMP_LOG
+ printf "%-75s%-15s\n" $STATUS_LINE_START "PASS" >> $TMP_LOG
 fi;
 
 cat $TMP_LOG
-rm $TMP_LOG
 
 if [ -f "$SCRIPT_OUT" ]; then
  sed 's/^/  >>>> /g' "$SCRIPT_OUT";
- rm "$SCRIPT_OUT"
 fi;
 
