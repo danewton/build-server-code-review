f9f906eacb1abca25f626904a4fdc343013096fa
Tim Vollmer <timjvollmer@gmail.com>
2014-03-31 10:58:10 -0500

    added rule to scan for System.err.println invocations.

---
+++ rules.d/src-should-not-have-system-err-printlns.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+. $(dirname $0)/../bin/setenv.sh
+
+if [ "$IS_MVN" == "false" ]; then
+ exit 0
+fi
+
+if [ "$TYPE" == "pom" ]; then
+ exit 0
+fi
+
+if [ $(grep -R 'System.err.println' $SRCDIR | grep -v '.svn' | wc -l) -ne 0 ]; then
+  echo "Please remove the System.err.println calls from the java source directory, and use a Logger to write to the application logs instead."
+  grep -nR 'System.err.println' $SRCDIR | grep -v '.svn'
+  exit 1
+fi
+
+
