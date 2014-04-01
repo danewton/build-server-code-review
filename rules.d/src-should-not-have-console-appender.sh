b679f8e3078e34130162a4dcd7ce8bafd17f5ed0
Tim Vollmer <timjvollmer@gmail.com>
2014-04-01 14:42:36 -0500

    exclude for .svn directories.

--- rules.d/src-should-not-have-console-appender.sh
+++ rules.d/src-should-not-have-console-appender.sh
@@ -10,9 +10,9 @@ if [ "$TYPE" == "pom" ]; then
  exit 0
 fi
 
-if [ "$(grep -c 'ConsoleAppender' $(find src/main -type f | egrep '.*log.*(\.xml|\.properties)'))" -ne 0 ]; then
+if [ "$(grep -c 'ConsoleAppender' $(find src/main -type f | grep -v '.svn' | egrep '.*log.*(\.xml|\.properties)'))" -ne 0 ]; then
   echo "Please remove the ConsoleAppender declaration from the java source directory (src/main)."
-  grep 'ConsoleAppender' $(find src/main -type f | egrep '.*log.*(\.xml|\.properties)')
+  grep 'ConsoleAppender' $(find src/main -type f | grep -v '.svn' | egrep '.*log.*(\.xml|\.properties)')
   exit 1
 fi
 
