8019660ff8a6913df500761cf36b1a85c3f4d64b
Tim Vollmer <timjvollmer@gmail.com>
2014-04-02 17:06:23 -0500

    added warning for IDE files.

---
+++ rules.d/project-should-not-have-ide-specific-files-committed.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+. $(dirname $0)/../bin/setenv.sh
+
+if [ "$IS_MVN" == "false" ]; then
+ exit 0
+fi
+
+if [ "$TYPE" == "pom" ]; then
+ exit 0
+fi
+
+MSG="Please remove your IDE specific settings from the project, so that other team members don't have to keep fighting with updates/commits simply because they are using a different IDE or a different version of the same IDE.  We would encourage you to use svn-ignores (or something comparable) to avoid checking these files in, in addition to any compiled code."
+
+if [ ! -d ".settings" ]; then
+ echo $MSG
+# exit 0
+fi
+
+if [ ! -d ".idea" ]; then
+ echo $MSG
+# exit 0
+fi
+
+if [ ! -d ".nb" ]; then
+ echo $MSG
+# exit 0
+fi
+
+if [ ! -d ".gradle" ]; then
+ echo $MSG
+# exit 0
+fi
