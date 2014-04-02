dee275ce1909bf5c7c348edb2e78cb0eea79efe0
Tim Vollmer <timjvollmer@gmail.com>
2014-04-02 17:21:14 -0500

    added a rule to check for svn directories on mvn-release plans.

---
+++ rules.d/svn-should-have-standard-directories.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+. $(dirname $0)/../bin/setenv.sh
+
+if [ "$IS_MVN" == "false" ]; then
+ exit 0
+fi
+
+IS_REL=$(isRelease $@)
+if [ "$IS_REL" != "true" ]; then
+ exit 0
+fi;
+
+SVN_SCM_URL=$(svn info | grep URL | cut -c 6-  | sed 's/\(.*\)\(tags\)\(.*\)/\1/g'  | sed 's/\(.*\)\(trunk\)\(.*\)/\1/g'  | sed 's/\(.*\)\(branches\)\(.*\)/\1/g')
+
+TMP_SVN_OUT=/tmp/svn-out$$
+
+function cleanUp(){
+ if [ -f "$TMP_SVN_OUT" ]; then
+  rm $TMP_SVN_OUT;
+ fi
+}
+
+trap "cleanUp" EXIT
+
+svn ls $SVN_SCM_URL > $TMP_SVN_OUT;
+
+if [ "$(grep -c 'branches' $TMP_SVN_OUT)" != 1 ]; then
+ echo "Please ensure that the SVN repo has the standard directorie of (trunk,tags,branches)."
+ exit 1
+fi;
+
+if [ "$(grep -c 'tags' $TMP_SVN_OUT)" != 1 ]; then
+ echo "Please ensure that the SVN repo has the standard directorie of (trunk,tags,branches)."
+ exit 1
+fi;
+
