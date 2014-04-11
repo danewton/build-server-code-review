423b725e09758a47ec8176525e4c6f4ebe352b44
Tim Vollmer <timjvollmer@gmail.com>
2014-04-10 23:07:30 -0500

    added additional check for spring version.

--- rules.d/check-spring-version.sh
+++ rules.d/check-spring-version.sh
@@ -9,6 +9,11 @@ fi
 MIN_SPRING_VERSION="2.5.6"
 rtn=0
 
+minVersion "spring" $MIN_SPRING_VERSION
+if [ $? -ne 0 ]; then
+ rtn=1
+fi
+
 minVersion "spring-core" $MIN_SPRING_VERSION
 if [ $? -ne 0 ]; then
  rtn=1
