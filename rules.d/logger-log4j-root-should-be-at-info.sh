80c86bed58adf962d935d2f8a01ca36b29787384
Tim Vollmer <timjvollmer@gmail.com>
2014-04-22 08:07:24 -0500

    added reference to notes for log4j configs.

--- rules.d/logger-log4j-root-should-be-at-info.sh
+++ rules.d/logger-log4j-root-should-be-at-info.sh
@@ -12,14 +12,14 @@ fi
 
 if [ -f "src/main/resources/log4j.properties" ]; then
   if [ $(grep -i 'log4j.rootLogger=INFO' src/main/resources/log4j.properties | wc -l) -ne 1 ]; then
-    echo 'Please place the root logger at INFO level : log4j.rootLogger=INFO'
+    echo 'Please place the root logger at INFO level : log4j.rootLogger=INFO. See http://en.wikipedia.org/wiki/Log4j regarding category -vs- logger.'
     exit 1
   fi
 fi
 
 if [ -f "src/main/resources/log4j.xml" ]; then
   if [ $(cat src/main/resources/log4j.xml  | perl -i -pe 'BEGIN{undef $/;} s/.*(<root>.*?<\/root>).*/\1/smg' | grep -i '<level value="info"/>' | wc -l) -ne 1 ]; then
-    echo 'Please place the root logger at INFO level : <level value="info"/>'
+    echo 'Please place the root logger at INFO level : <level value="info"/>. See http://en.wikipedia.org/wiki/Log4j regarding priority -vs- level.'
     exit 1
   fi
 fi
