a7f50ab62f2a1f41c2fc5a2c64b01408d31a332d
Tim Vollmer <timjvollmer@gmail.com>
2014-06-09 12:47:32 -0500

    corrected bug that caused script to hang.

--- rules.d/check-jquery-version.sh
+++ rules.d/check-jquery-version.sh
@@ -19,13 +19,11 @@ isAfter $FAIL_DATE
 if [ $? -eq 0 ]; then
   ERR_RSLT=$EXIT_WARN
 fi
-  
-if [ $(grep -R 'jquery' $WEBDIR | grep -E '(script|link|href)' | grep -E '(.*[0-9]\.[0-9]\.[0-9].*)' | perl -i -pe 's/(.*?(\d\.\d\.\d).*)/\2:\1/g' | cut -d: -f1,2 | sort -u | grep -Ev '^(1\.7|1\.8|1\.9|2\.4)' | grep -Ev '^1\.6\.[3-9]' | grep -v '.svn' | wc -l) -ne 0 ]; then
-  echo "Please upgrade your version of jquery.  JQuery versions 1.6.2 and older have been identified as having PCI vulnerabilities (see http://web.nvd.nist.gov/view/vuln/detail?vulnId=CVE-2011-4969). $RULE_MSG"
-  grep -nR 'jquery' $WEBDIR | grep -E '(script|link|href)' | grep -E '(.*[0-9]\.[0-9]\.[0-9].*)' | perl -i -pe 's/(.*?(\d\.\d\.\d).*)/\2:\1/g' | cut -d: -f1,2 | sort -u | grep -Ev '^(1\.7|1\.8|1\.9|2\.4)' | grep -Ev '^1\.6\.[3-9]' | grep -v '.svn'
-  exit $ERR_RSLT
-fi  
-    
 
+if [ $(grep -R 'jquery' $WEBDIR | grep -E '(script|link|href)' | grep -E '(.*[0-9]+\.[0-9]+\.[0-9]+.*)' | perl -i -pe 's/(.*?(\d+\.\d+\.\d)+.*)/\2:\1/g' | cut -d: -f1,2 | sort -u | grep -Ev '^(1\.7|1\.8|1\.9|2\.4)' | grep -Ev '^1\.6\.[3-9]' | grep -v '.svn' | wc -l) -ne 0 ]; then
+  echo "Please upgrade your version of jquery.  JQuery versions 1.6.2 and older have been identified as having PCI vulnerabilities (see http://web.nvd.nist.gov/view/vuln/detail?vulnId=CVE-2011-4969). $RULE_MSG"
+  grep -nR 'jquery' $WEBDIR | grep -E '(script|link|href)' | grep -E '(.*[0-9]+\.[0-9]+\.[0-9]+.*)' | perl -i -pe 's/(.*?(\d+\.\d+\.\d+).*)/\2:\1/g' | cut -d: -f1,2 | sort -u | grep -Ev '^(1\.7|1\.8|1\.9|2\.4)' | grep -Ev '^1\.6\.[3-9]' | grep -v '.svn'
+  exit $ERR_RSLT
+fi
 
 
