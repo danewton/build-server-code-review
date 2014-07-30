710d8b5f017cb402acb7d34450843d02c2e398d4
Tim Vollmer <timjvollmer@gmail.com>
2014-07-30 09:48:25 -0500

    fixed rule for validating jquery version.

--- rules.d/check-jquery-version.sh
+++ rules.d/check-jquery-version.sh
@@ -31,9 +31,9 @@ if [ "$ARTIFACT_ID" == "edsweb" ]; then
   fi
 fi
 
-if [ $(grep -R 'jquery' $WEBDIR | grep -E '(script|link|href)' | grep -Ev '(.svn|dcmegamenu|migrate|multi-accordion|jquery-ui)' | grep -E '(.*[0-9]+\.[0-9]+\.[0-9]+.*)' | perl -i -pe 's/(.*?(\d+\.\d+\.\d)+.*)/\2:\1/g' | cut -d: -f1,2 | sort -u | grep -Ev '^(1\.7|1\.8|1\.9|1\.10|1\.11|2\.4)' | grep -Ev '^1\.6\.[3-9]' | wc -l) -ne 0 ]; then
+if [ $(grep -R 'jquery' $WEBDIR | grep -E '(script|link|href)' | grep -Ev '(.svn|dcmegamenu|migrate|multi-accordion|jquery-ui)' | grep -E '(.*[0-9]+\.[0-9]+\.[0-9]+.*)' | perl -i -pe 's/(.*?(\d+\.\d+\.\d)+.*)/\2:\1/g' | cut -d: -f1,2 | sort -u | grep -Ev '^(1\.7|1\.8|1\.9|1\.10|1\.11|2\.0|2\.1\|2\.4)' | grep -Ev '^1\.6\.[3-9]' | wc -l) -ne 0 ]; then
   echo "Please upgrade your version of jquery.  JQuery versions 1.6.2 and older have been identified as having PCI vulnerabilities (see http://web.nvd.nist.gov/view/vuln/detail?vulnId=CVE-2011-4969). $RULE_MSG"
-  grep -nR 'jquery' $WEBDIR | grep -E '(script|link|href)' | grep -Ev '(.svn|dcmegamenu|migrate|multi-accordion|jquery-ui)' | grep -E '(.*[0-9]+\.[0-9]+\.[0-9]+.*)' | perl -i -pe 's/(.*?(\d+\.\d+\.\d)+.*)/\2:\1/g' | cut -d: -f1,2 | sort -u | grep -Ev '^(1\.7|1\.8|1\.9|1\.10|1\.11|2\.4)' | grep -Ev '^1\.6\.[3-9]'
+  grep -nR 'jquery' $WEBDIR | grep -E '(script|link|href)' | grep -Ev '(.svn|dcmegamenu|migrate|multi-accordion|jquery-ui)' | grep -E '(.*[0-9]+\.[0-9]+\.[0-9]+.*)' | perl -i -pe 's/(.*?(\d+\.\d+\.\d)+.*)/\2:\1/g' | cut -d: -f1,2 | sort -u | grep -Ev '^(1\.7|1\.8|1\.9|1\.10|1\.11|2\.0|2\.1\|2\.4)' | grep -Ev '^1\.6\.[3-9]'
   exit $ERR_RSLT
 fi
 
